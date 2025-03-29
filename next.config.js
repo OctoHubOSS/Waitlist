ade93f2df702a954bf7dcb7309a6962d57144e36
TheRealToxicDev <toxic.dev09@gmail.com>
2025-03-29 04:22:11 -0600

    feat(update): stuff and things

--- next.config.js
+++ next.config.js
@@ -18,10 +18,10 @@ const nextConfig = {
   },
   async redirects() {
     // Only apply redirects in production
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'development') {
       return [
         {
-          source: '/:path((?!coming-soon|api|_next|static|waitlist/unsubscribe|waitlist/subscribe).*)*',
+          source: '/:path((?!coming-soon|api|_next|static|public|waitlist/unsubscribe|waitlist/subscribe).*)*',
           destination: '/coming-soon',
           permanent: true,
         }
